d3d421f8e4a3bfad6c0c8b2134e9221c315d7712
gershik <me@gershun.in>
2022-11-06 18:56:23 +0200

    new stuff.
    Co-authored-by: Umar Getagazov <umar@handlerug.me>

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-function openSpoiler(whichIsOpen) {
-    document.querySelector("#links").style.display = "none";
-    document.querySelector(whichIsOpen).style.display = "block";
-}
-
-function goBack(whichIsOpen) {
-    document.querySelector("#links").style.display = "block";
-    document.querySelector(whichIsOpen).style.display = "none";
-
-}
-
 (function() {
     let birthday = new Date(2002, 10, 25);
     let date = new Date();
