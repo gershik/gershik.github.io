de78ddadc3baafa5b254ba5b19b1fa66f0036750
gershik <me@gershun.in>
2022-11-25 16:47:53 +0200

    fix script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-(function() {
+window.onload = (function() {
     let birthday = new Date(2002, 10, 25);
     let date = new Date();
     let now = new Date(date.getFullYear(), date.getMonth(), date.getDate());
